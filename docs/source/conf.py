6c9bc04a4b3633c4108539115ae72a8c1b55cbea
widberg <jaredwidberg@gmail.com>
2021-06-21 20:12:47 -0400

    stuffs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ project = 'FMTK'
 copyright = '2021, widberg'
 author = 'widberg'
 
-with open('../CMakeLists.txt') as f:
+with open('../../CMakeLists.txt') as f:
     for line in f.readlines():
         m = re.match(r'^project\(fmtk.+VERSION\s+(\d+\.\d+\.\d+\.\d+).+\)$', line)
         if m:
